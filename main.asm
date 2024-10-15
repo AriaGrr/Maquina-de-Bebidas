a770761f9e3fd1e2d9cf474de065f6466f38725d
Nuno Martins <145809068+nunomgs136@users.noreply.github.com>
2024-10-15 18:30:07 -0300

    LCD terminado

--- main.asm
+++ main.asm
@@ -95,7 +95,7 @@ escreveString:
 MOV R2, #0
 rot:
 MOV A, R2
-MOVC A,@A+DPTR ;lê a tabela da memória de programa
+MOVC A,@A+DPTR ;lÃª a tabela da memÃ³ria de programa
 ACALL sendCharacter ; send data in A to LCD module
 INC R2
 JNZ rot ; if A is 0, then end of data has been reached - jump out of loop
@@ -128,7 +128,7 @@ CALL delay
 RET
 
 
-;Limpa todo o display e retorna o cursor para primeira posição
+;Limpa todo o display e retorna o cursor para primeira posiÃ§Ã£o
 clearDisplay :
 CLR P1.3 ; clear RS - indicates that instruction is being sent to module
 CLR P1.7 ; |
@@ -147,7 +147,7 @@ CALL delay ; wait for BF to clear
 RET
 
 
-;Retorna o cursor para primeira posição sem limpar o display
+;Retorna o cursor para primeira posiÃ§Ã£o sem limpar o display
 retornaCursor :
 CLR P1.3 ; clear RS - indicates that instruction is being sent to module
 CLR P1.7 ; |
@@ -165,27 +165,56 @@ CLR P1.2 ; | negative edge on E
 CALL delay ; wait for BF to clear
 RET
 
-FEI:
-DB "CC-4.50  Spr-6.00"
+COCA:
+DB "Coca Cola - 3.50"
 DB 0 ;caracter null indica fim da String
-Display:
-DB "P-3.50 Mons-7.00"
+SPRITE:
+DB "Sprite - 3.50"
+DB 0
+MONSTER: 
+DB "Monster - 7.50"
+DB 0
+PEPSI:
+
+DB "Pepsi - 3.50"
 DB 0
 
 main:
 ACALL lcd_init
 MOV A, #00h
 ACALL posicionaCursor
-MOV DPTR,#FEI ;DPTR = inicio
+MOV DPTR,#COCA ;DPTR = inÃ­cio da palavra FEI
 ACALL escreveString
 MOV A, #40h
 ACALL posicionaCursor
-MOV DPTR,#Display ;DPTR = inicio
+MOV DPTR,#PEPSI ;DPTR = inÃ­cio da palavra Display
 ACALL escreveString
-JMP $
+
+ACALL delay
+ACALL clearDisplay
+ACALL delay
+MOV A, #00h
+
+ACALL posicionaCursor
+
+MOV DPTR, #SPRITE
+ACALL delay
+ACALL escreveString
+MOV A, #40h
+ACALL posicionaCursor
+MOV DPTR, #MONSTER
+ACALL escreveString
+ACALL clearDisplay 
+JMP main
 
 
 delay:
+MOV R1, #50
 MOV R0, #50
+MOV R3, #50
+MOV R4,	#50
 DJNZ R0, $
+DJNZ R1, $
+DJNZ R3, $
+DJNZ R4, $
 RET
